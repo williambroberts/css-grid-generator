6a487f7a437f40c9e3196f42a76337cce2764363
= <=3williamroberts@gmail.com>
2023-05-12 13:33:04 +0100

    mark 20

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
+'use client'
+import React, { useEffect,useState } from 'react'
 import Self from '../self/self'
 import ThemeButton from '../themes/themeButton'
 import Hamburger from './hamburger'
 
 const Header = () => {
+  const [isHamburger,setIsHamburger]=useState(false)
+
+  useEffect(()=>{
+    let htmlTag = document.querySelector('html')
+     htmlTag.style.overflowY= isHamburger?'hidden':'scroll'
+  },[isHamburger])
   return (
    <header>
     <nav className='header-nav'>
         <span className='header-self'><Self/></span>
         <span className='header-center'>CSS Grid Generator</span>
         <span className='header-mid'><ThemeButton/></span>
-        <span className='header-right'>☰</span>
+        <span className='header-right' onClick={()=>setIsHamburger((prev)=>!prev)}>☰</span>
     </nav>
+    <div className={`hamburger-container ${isHamburger? 'open':''}`}>
     <Hamburger/>
-    <div className='hamburger-blur'></div>
+    <div className='hamburger-blur' onClick={()=>setIsHamburger((prev)=>!prev)}></div>
+    </div>
+   
    </header>
   )
 }
