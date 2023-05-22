1813a4b2fb774c1b4e2fea133660601a57431978
= <=3williamroberts@gmail.com>
2023-05-22 11:27:13 +0100

    e 1

--- app/about/loading.jsx
+++ app/about/loading.jsx
@@ -3,7 +3,7 @@ import IconLoading3Quarters from "@/components/icons/loading/loading";
 const LoadingPage = () => {
     return (
       <div className='loader'>
-        <div className="half-circle-one">
+        {/*<div className="half-circle-one">
           <div className="half-circle-two">
             <div className="half-circle-three">
               <IconLoading3Quarters/>
@@ -11,7 +11,13 @@ const LoadingPage = () => {
                 
           </div>
         
-        </div>
+        </div>*/}
+       <div className='sq-flex'>
+        <span className='sq-1'></span>
+        <span className='sq-2'></span>
+        <span className='sq-3'></span>
+        <span className='sq-4'></span>
+      </div>
       </div>
     );
   };
